1ab5663e4d89fec4a6b2f3b7a9312582c08c2ccc
tjahan805 <110358481+tjahan805@users.noreply.github.com>
2022-10-04 18:09:48 -0400

    ver1

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -10,16 +10,16 @@ public class TipCalc
         Scanner money = new Scanner (System.in);
         System.out.print("Enter total bill amount: ");
         double firstTotalBill = money.nextDouble();
-        System.out.print("Enter preferred tip percentage: ");
+        System.out.print("Enter preferred tip percentage (whole numbers only): ");
         int tipPercent = money.nextInt();
         System.out.print("Enter number of people at party: ");
-        int numofPeople = money.nextInt();
+        int numOfPeople = money.nextInt();
 
         //declare the outputs needed by user
-        double tipAmount = firstTotalBill / ((double) tipPercent);
+        double tipAmount = firstTotalBill * (tipPercent*.01);
         double secondTotalBill = firstTotalBill + tipAmount;
-        double tipPerPerson = tipAmount / ((double) numofPeople);
-        double totalPerPerson = secondTotalBill / ((double) numofPeople);
+        double tipPerPerson = tipAmount / numOfPeople;
+        double totalPerPerson = secondTotalBill / numOfPeople;
 
         //format all the calculated outputs to rounded cents
         DecimalFormat df = new DecimalFormat(".##");
@@ -28,7 +28,7 @@ public class TipCalc
         System.out.println();
         System.out.println("Total Bill Before Tip: " + firstTotalBill);
         System.out.println("Tip Percentage: " + tipPercent + "%");
-        System.out.println("Number of People: " + numofPeople);
+        System.out.println("Number of People: " + numOfPeople);
         System.out.println();
         System.out.println("Tip Amount: " + df.format(tipAmount));
         System.out.println("Tip Per Person: " + df.format(tipPerPerson));
